436a40dd706a4af137489c0440444cc2945d77c0
arushi1998 <arushi.sajwan@gmail.com>
2019-02-08 17:24:26 +0530

    commit1

---
+++ app/src/main/java/com/example/arush/healthapptechwarrior/steps.java
@@ -0,0 +1,13 @@
+package com.example.arush.healthapptechwarrior;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class steps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_steps);
+    }
+}
